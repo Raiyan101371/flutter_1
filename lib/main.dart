18d20f1cbe1fcb1e7226f4ed6040ce94bdec1c9b
Raiyan101371 <rahman15-5508@diu.edu.bd>
2025-02-26 10:36:53 +0600

    lab06-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Lab06());
+  runApp(const advance_ui());
 }
-/*
-tinyurl.com/cse414-nike-res
-*/
-class Lab06 extends StatelessWidget {
-  const Lab06({super.key});
+
+class advance_ui extends StatelessWidget {
+  const advance_ui({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "this is new app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter UI 2"),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Advance UI-UX"),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(124, 212, 432, 123),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network("https://i.quotev.com/klqyz2bgaaaa.jpg"),
+            Image.network(
+                "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest/scale-to-width-down/300?cb=20230803224121"),
+            CachedNetworkImage(
+              imageUrl: "http://via.placeholder.com/350x150",
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ],
         ),
-        body: Container(
-          height:double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.star,
-                color: Colors.yellow,
-                size:60,
-              ),
-              Icon(
-                Icons.star,
-                color: Colors.yellow,
-                size:60,
-              ),
-            ]
-          ) 
-        ,
-      ),//Row
-    )
-    );
+      ),
+    ));
   }
 }
